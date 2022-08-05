d25f94da4d53929fae9d4a61cb493b1016acd53d
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2022-08-05 10:04:23 +0000

    remove unecessary field

--- src/stores/things.ts
+++ src/stores/things.ts
@@ -1,10 +1,9 @@
 import { writable, type Writable } from 'svelte/store'
 
 interface Thing {
-       name: string
-    earning: number
-          x: number
-          y: number
+      name: string
+         x: number
+         y: number
 }
 type ThingsType = Thing[]
 export const things: Writable<ThingsType> = writable([])
