c12f08aa68d434aeff6bc63cfa62792cb70b8c37
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2022-08-12 11:44:57 +0000

    Add (right now) Unfinished Menu

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
     import { createEventDispatcher } from 'svelte'
+    import { scale } from 'svelte/transition'
     import type { ThingResult } from '../services/things'
     const dispatch = createEventDispatcher()
 
@@ -7,3 +8,25 @@
         dispatch(res)
     }
 </script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,100,0,0" />
+</svelte:head>
+
+<div class='p-2 pr-4 w-fit rounded bg-dark text-white font-light flex gap-1 flex-col' 
+     transition:scale='{{ duration: 200 }}'>
+    <button class='flex gap-1'>
+        <span class='material-symbols-outlined'>Open_With</span>
+        Move
+    </button>
+
+    <button class='flex gap-1'>
+        <span class='material-symbols-outlined'>Bar_Chart</span>
+        Statistics
+    </button>
+
+    <button class='flex gap-1 text-red-500'>
+        <span class='material-symbols-outlined'>Delete</span>
+        Delete
+    </button>
+</div>
--- src/components/Thing.svelte
+++ src/components/Thing.svelte
@@ -1,4 +1,6 @@
 <script lang='ts'>
+    import Menu from "./Menu.svelte"
+
     export let x: number,
                y: number,
                name: string,
@@ -7,15 +9,22 @@
     // meta.then(v => console.log(v.levels[0]))
     const thing = meta.then(v => import(`../assets/thing/${v.levels[level].src}.png`))
 
-    function openMenu() {
-
+    let isMenuOpen = false
+    function toggleMenu() {
+        isMenuOpen = !isMenuOpen
     }
 </script>
 
-<span style='grid-column: {x}; grid-row: {y}'
-      on:click={openMenu}>
+<span style='grid-column: {x}; grid-row: {y}'>
     {#await thing then thingImg}
         <!-- svelte-ignore a11y-missing-attribute -->
-        <img src={thingImg.default} class='w-8 scale-crisp'>
+        <img src={thingImg.default} class='w-8 scale-crisp' on:click={toggleMenu}>
     {/await}
+    
 </span>
+
+{#if isMenuOpen}
+    <span style='transform: translate({x * 32 + 5}px, {(y - 3) * 32}px);'>
+        <Menu/>
+    </span>
+{/if}
