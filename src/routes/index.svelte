97e737e508085eaaaa50b4e175e1f893f607bb3a
a-username-is-available <lucas.liu20@education.nsw.gov.au>
2022-09-14 02:22:46 +0000

    Change Font Back + Make Play Button Work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
-<h1>Hello it's the game thing!!</h1>
+<div class='h-screen w-screen bg-lime-300 flex justify-center flex-col'>
+    <img class='scale-crisp scale-50' src="/src/assets/logo.png" alt="Logo">
+    <!--  left-50  -->
+    <span class='absolute justify-self-center' style='transform: translate(50%, 100%)'>
+        <p>Hi! Are you feeling down? Try this game and create the world you like.</p>
+        <button class='bg-secondary border-primary border-solid rounded px-10 py-5 w-fit'
+                on:click={() => window.location.href = '/game'}>Play ></button>
+    </span>
+</div>
